759f95af4000ecab2d5e022b1f123c33ac34a9f4
David Miller <58112304+mill3rd@users.noreply.github.com>
2025-06-16 09:19:00 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// GitHub Configuration for Leaderboard
+// REPLACE THESE VALUES WITH YOUR ACTUAL GITHUB INFO
+
+var GITHUB_USERNAME = 'mill3rd';  // Replace with your GitHub username
+var GITHUB_REPO = 'republic-quiz';             // This should match your repo name
+var GITHUB_TOKEN = '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';        // Replace with your personal access token
