65a5becc30c71f09f78e88ee748e6d58a0bedc56
David Miller <58112304+mill3rd@users.noreply.github.com>
2025-06-16 10:06:18 +0100

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,3 @@ function skipToBonus() {
     // Show the main completion screen
     showMainComplete();
 }
-
-var GITHUB_USERNAME = 'mill3rd';  // Replace with your GitHub username
-var GITHUB_REPO = 'republic-quiz-scores';             // This should match your repo name
-var GITHUB_TOKEN = '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';        // Replace with your personal access token
