733f1eae932ae5d7d3ce4b7570017c69da5c62ae
David Miller <58112304+mill3rd@users.noreply.github.com>
2025-06-16 10:13:44 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,1 @@
-// Hidden test function - skip to bonus rounds
-function skipToBonus() {
-    console.log('skipToBonus called!'); // Debug line
-    // Set fake perfect score for testing
-    score = 23;
-    currentQuestion = 23;
-    
-    // Update the score display
-    updateScore();
-    
-    // Show the main completion screen
-    showMainComplete();
-}
+
